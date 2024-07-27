e12ebc95aead33d14e762c32615de5f184a36f3b
Subhajit Chowdhury <dev.subhajitchow@gmail.com>
2024-07-27 12:44:12 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# task-mcmakler-playwright-august22
+
+Task project for McMakler company: automated webapp testing with Javascript and Playwright framework.
+
+---
+
+Recommended Node.js version: 16.16.0
+
+Recommended NPM version: 8.18.0
+
+Node.js packages used: Playwright 1.25.0, expect-playwright 0.8.0, axe-core 4.4.3, prettier 2.7.1
+
+---
+
+**How to run automation:**
+You will need already installed Node.js and NPM packages into system. Please check this link for more details: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm/
+
+Recommended to use any terminal app that you prefer.
+
+1. Go to project folder and run packages installation with command: `npm install`
+2. Now you able to run automation with command: `npx playwright test`
+3. After automation run is finished you able to see results in your terminal output.
+   Additionally you can run command `npx playwright show-report` to see results in browser.
+
+---
+
+Webapp under test: https://admin-advertisement.herokuapp.com
+
+Webapp's API under test: https://admin-advertisement.herokuapp.com/api/advertisements/
+
+**Testing goal:**
+
+We have two flows:
+
+1. Create new advertisement
+2. Edit any existing advertisement
+
+What you need to do → API + E2E testing:
+
+1. Create test cases for flow above
+2. Implement scenarios for created cases
+
+**Project conditions:**
+* Javascript/Typescript are the preferred programming languages
+* Use any Framework you’re comfortable with
+* Use any structure or pattern you’re comfortable with
+* Include a README file, in which you explain how to set up the project, how to run
+tests etc.
+
+---
+
+E2E functional test suite - `tests/e2e.spec.js`
+
+UI functional test suite - `tests/ui.spec.js`
+
+API functional test suite - `tests/api.spec.js`
+
+19 automated tests prepared for this project.
+
+---
+
+Company's feedback which was received:
+
+"The page object model, wasn't fully used, only selectors in the page file. No helpers for API functions. Test files are too big to be easily understood. Test data is not separated from the logic. Playwright was not correctly configured, the browser was running in non-headless mode."
---
+++ tests/ui.spec.js
@@ -0,0 +1,167 @@
+const { test, expect } = require("@playwright/test");
+const AxeBuilder = require("@axe-core/playwright").default;
+const { MainPage } = require("../lib/pageobjects/LoginPage");
+
+test.beforeAll(async ({ request }) => {
+    const req = await request.get("/api/advertisements/db/drop?confirm=y");
+    expect(req.ok()).toBeTruthy();
+});
+
+test("UI accessibility test", async ({ page }, testInfo) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+
+    const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
+
+    await testInfo.attach("accessibility-scan-results", {
+        body: JSON.stringify(accessibilityScanResults, null, 2),
+        contentType: "application/json",
+    });
+
+    expect(accessibilityScanResults.violations).toEqual([]);
+});
+
+test("Page main elements in place", async ({ page }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+    await expect(mainPage.appNameText).toBeVisible();
+    await expect(mainPage.newItemButton).toBeVisible();
+    await expect(mainPage.tableHeader.name).toBeVisible();
+    await expect(mainPage.tableHeader.street).toBeVisible();
+    await expect(mainPage.tableHeader.rooms).toBeVisible();
+    await expect(mainPage.tableHeader.price).toBeVisible();
+    await expect(mainPage.tableHeader.status).toBeVisible();
+    await mainPage.newItemButton.click();
+    await expect(mainPage.editForm.name).toBeVisible();
+    await expect(mainPage.editForm.street).toBeVisible();
+    await expect(mainPage.editForm.rooms).toBeVisible();
+    await expect(mainPage.editForm.price).toBeVisible();
+    await expect(mainPage.editForm.status).toBeVisible();
+    await expect(mainPage.cancelBtn).toBeEnabled();
+    await expect(mainPage.saveBtn).toBeDisabled();
+});
+
+test.describe("UI: Create new advertisement", async () => {
+    test("New element with required fields only", async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.goto();
+
+        const reqName = "ui_name";
+        const reqPrice = "555";
+
+        await mainPage.newItemButton.click();
+        await mainPage.editForm.name.fill(reqName);
+        await mainPage.editForm.price.fill(reqPrice);
+        await mainPage.saveBtn.click();
+        await mainPage.savePopUp.waitFor({
+            state: "visible",
+        });
+        await mainPage.newItemButton.waitFor();
+        let rows = mainPage.tableRows;
+        let item = rows.filter({ hasText: "ui_name" }).filter({ hasText: "555" });
+        let itemTexts = await item.allTextContents();
+        expect(itemTexts[0]).toContain(reqName);
+        expect(itemTexts[0]).toContain(reqPrice);
+    });
+    test("New element with all fields", async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.goto();
+
+        const reqName = "ui_name";
+        const reqStreet = "ui_street";
+        const reqRooms = "393";
+        const reqPrice = "7777";
+        const reqStatus = "Active";
+
+        await mainPage.newItemButton.click();
+        await mainPage.editForm.name.fill(reqName);
+        await mainPage.editForm.street.fill(reqStreet);
+        await mainPage.editForm.rooms.fill(reqRooms);
+        await mainPage.editForm.price.fill(reqPrice);
+        await mainPage.editForm.status.check();
+        await mainPage.saveBtn.click();
+        await mainPage.savePopUp.waitFor({
+            state: "visible",
+        });
+        await mainPage.newItemButton.waitFor();
+        let rows = mainPage.tableRows;
+        let item = rows.filter({ hasText: "ui_name" }).filter({ hasText: "ui_street" });
+        let itemTexts = await item.allTextContents();
+        expect(itemTexts[0]).toContain(reqName);
+        expect(itemTexts[0]).toContain(reqStreet);
+        expect(itemTexts[0]).toContain(reqRooms);
+        expect(itemTexts[0]).toContain("7.777,00");
+        expect(itemTexts[0]).toContain(reqStatus);
+    });
+});
+
+test("Edit any existing advertisement", async ({ page }) => {
+    const mainPage = new MainPage(page);
+    await mainPage.goto();
+
+    const reqName = "new_name";
+    const reqPrice = "343";
+    const editName = "edit_name";
+    const editPrice = "901";
+
+    await mainPage.newItemButton.click();
+    await mainPage.editForm.name.fill(reqName);
+    await mainPage.editForm.price.fill(reqPrice);
+    await mainPage.saveBtn.click();
+    await mainPage.savePopUp.waitFor({
+        state: "visible",
+    });
+    await mainPage.newItemButton.waitFor();
+    let rows = mainPage.tableRows;
+    let item = rows.filter({ hasText: reqName }).filter({ hasText: reqPrice });
+    let itemTexts = await item.allTextContents();
+    expect(itemTexts[0]).toContain(reqName);
+    expect(itemTexts[0]).toContain(reqPrice);
+
+    await item.click();
+    await mainPage.editForm.name.fill(editName);
+    await mainPage.editForm.price.fill(editPrice);
+    await mainPage.saveBtn.click();
+    await mainPage.savePopUp.waitFor({
+        state: "visible",
+    });
+    await mainPage.newItemButton.waitFor();
+    let editRows = mainPage.tableRows;
+    let editItem = editRows.filter({ hasText: editName }).filter({ hasText: editPrice });
+    let editItemTexts = await editItem.allTextContents();
+    expect(editItemTexts[0]).toContain(editName);
+    expect(editItemTexts[0]).toContain(editPrice);
+});
+
+test.describe("UI: Input validators", async () => {
+    test("Advertisement name", async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.goto();
+        await mainPage.newItemButton.click();
+
+        await mainPage.editForm.name.fill("111111111111111111111111111111111111111111111111111");
+        await mainPage.editForm.street.click();
+        await expect(page.locator('text="Max length reached"').first()).toBeVisible();
+        await expect(mainPage.saveBtn).toBeDisabled();
+
+        await mainPage.editForm.name.fill("");
+        await mainPage.editForm.street.click();
+        await expect(page.locator('text="This is required"').first()).toBeVisible();
+        await expect(mainPage.saveBtn).toBeDisabled();
+    });
+    test("Price", async ({ page }) => {
+        const mainPage = new MainPage(page);
+        await mainPage.goto();
+        await mainPage.newItemButton.click();
+
+        await mainPage.editForm.price.fill("565,3411");
+        await mainPage.editForm.street.click();
+        await expect(page.locator('text="Invalid price(Valid currency in euros: 12,12)"').first()).toBeVisible();
+        await expect(mainPage.saveBtn).toBeDisabled();
+
+        await mainPage.editForm.price.fill("");
+        await mainPage.editForm.street.click();
+        await expect(page.locator('text="This is required"').first()).toBeVisible();
+        await expect(mainPage.saveBtn).toBeDisabled();
+    });
+});
